ced28db537c0f23b071d1f3e3c31bd5a156b5420
Altun98 <92326419+Altun98@users.noreply.github.com>
2025-03-13 21:17:57 +0400

    test sd

--- ConsoleApp1/AppDbContext.cs
+++ ConsoleApp1/AppDbContext.cs
@@ -12,6 +12,9 @@ namespace ConsoleApp1
         public DbSet<Employee> Employees { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
+            //sdsdsdsd
+            // sdsdd
+            int a = 12;
             optionsBuilder.UseSqlServer("Server=CAHID\\MY_DB;Database=DB_EF;User Id=sa;Password=2016;TrustServerCertificate=True;");
         }
     }
